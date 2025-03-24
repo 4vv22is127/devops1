d0821b4d499e99dcf5a1e3b42e884bad646c1e5f
4vv22is127 <vvce22me0046@vvce.ac.in>
2025-03-24 15:15:58 +0530

    Update AsciiValue2.java

--- AsciiValue2.java
+++ AsciiValue2.java
@@ -3,11 +3,11 @@ public class AsciiValue2 {
     public static void main(String[] args) {
 
         char ch = 'a';
-        int ascii = ch;
+        int castAscii = ch;
        
         int  = (int) ch;
 
         System.out.println("The ASCII value of " + ch + " is: " + ascii);
-        System.out.println("The ASCII value of " + ch + " is: " + Ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
     }
 }
